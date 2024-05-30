5db836d57281754b1a4b64ede4728d484b9d6905
Nico Muszalczyk <74101214+starsbit@users.noreply.github.com>
2024-05-30 12:02:41 +0200

    use back icon in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,6 +14,7 @@ import { ThemeService } from '../../services/theme.service';
 })
 export class HeaderComponent {
   @Input() route: string | null = null;
+  @Input() backIcon = false;
 
   copyrightYear = new Date().getFullYear();
 
